a15a54fb9052d1bf6875e27fc5735306c11a7e75
Xelvaris-kz <kinhighman@gmail.com>
2025-05-31 01:31:01 +0500

    Update 15_levels_and_ranks.cpp

--- 3-conditionals/15_levels_and_ranks.cpp
+++ 3-conditionals/15_levels_and_ranks.cpp
@@ -4,21 +4,24 @@
 #include <iostream>
 
 int main() {
-  int levels = 11;
-
-  if (levels < 6) {
-    std::cout << "🥉 Bronze\n";
-  }
-  else if (levels < 11) {
-    std::cout << "🥈 Silver\n";
-  }
-  else if (levels < 16) {
-    std::cout << "🥇 Gold\n";
-  }
-  else if (levels < 21) {
-    std::cout << "🏅 Platinum\n";
-  }
-  else {
-    std::cout << "💎 Diamond\n";
-  }
+  // Write code here
+  int level;
+  std::cout << "Enter your level: ";
+  std::cin >> level;
+  
+  if (level <= 5) {
+  std::cout << "🥉Bronze\n";
+}
+  else if (level <= 10) {
+  std::cout << "🥈Silver\n";
+}
+  else if (level <= 15) {
+  std::cout << "🥇Gold\n";
+}
+  else if (level <= 20) {
+  std::cout << "🏅Platinum\n";
+}
+  else if (level <= 25) {
+  std::cout << "💎Diamond\n";
+}
 }
